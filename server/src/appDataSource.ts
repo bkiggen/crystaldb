128dd1a5dbc4c92090cbf22eb096bb7ab3063ce0
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2024-06-13 07:55:47 -0700

    stop syncing changes

--- server/src/appDataSource.ts
+++ server/src/appDataSource.ts
@@ -1,18 +1,18 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm'
 
 // put these in .env
 export const appDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   // logging: true,
   // logger: "advanced-console",
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
-  entities: ["src/entity/**/*.ts"],
-});
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  entities: ['src/entity/**/*.ts'],
+})
